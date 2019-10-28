8362755d9e7efb3da532d79141c3602c4f0926d5
unknown <ultraxia@foxmail.com>
2019-10-29 00:21:25 +0800

    first commit

---
+++ AutoTicket.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tebeka/selenium"
+	"github.com/tebeka/selenium/chrome"
+	"log"
+)
+
+func main() {
+	opts := []selenium.ServiceOption{}
+	caps := selenium.Capabilities{
+		"browserName": "chrome",
+	}
+
+	// 禁止加载图片，加快渲染速度
+	imagCaps := map[string]interface{}{
+		"profile.managed_default_content_settings.images": 0,
+	}
+
+	chromeCaps := chrome.Capabilities{
+		Prefs: imagCaps,
+		Path:  "",
+		Args: []string{
+			//"--headless", // 设置Chrome无头模式
+			"--no-sandbox",
+			"--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7", // 模拟user-agent，防反爬
+		},
+	}
+	caps.AddChrome(chromeCaps)
+	// 启动chromedriver，端口号可自定义
+	_, err := selenium.NewChromeDriverService("./chromedriver", 9515, opts...)
+	if err != nil {
+		log.Printf("Error starting the ChromeDriver server: %v", err)
+	}
+	// 调起chrome浏览器
+	webDriver, err := selenium.NewRemote(caps, fmt.Sprintf("http://localhost:%d/wd/hub", 9515))
+	if err != nil {
+		panic(err)
+	}
+
+	webDriver.AddCookie(&selenium.Cookie{
+		Name:  "defaultJumpDomain",
+		Value: "www",
+	})
+	// 导航到目标网站
+	err = webDriver.Get("http://www.github.com")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to load page: %s\n", err))
+	}
+	log.Println(webDriver.Title())
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GoTicket
+
+
+
+
+
+## 更新记录
+**2019.10.29更新**：新建仓库，使用Golang调用ChromeDriver成功
